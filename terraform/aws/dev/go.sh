dd1413693323c728c0fba709143d53df08ae6df4
Jorge Moratilla <jorge@moratilla.com>
2017-01-22 13:40:03 +0100

    terraform.tfvars variables must be commented

--- terraform/aws/dev/go.sh
+++ terraform/aws/dev/go.sh
@@ -7,4 +7,6 @@ sudo apt-get -yy install git puppet
 git clone https://github.com/devops-studentXX/puppet.git
 
 cd puppet
-sudo puppet apply init.pp --modulepath modules
+bundle
+librarian-puppet install --clean
+sudo FACTER_app_version=$APP_VERSION FACTER_app_repository=$APP_REPOSITORY puppet apply init.pp --modulepath modules
